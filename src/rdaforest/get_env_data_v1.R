a90e979c3b036c138fabaa6600713ff94b5f7b6d
Enrico Bazzicalupo <enricobazzical@gmail.com>
2025-11-12 11:24:27 +0100

    more samples

--- src/rdaforest/get_env_data_v1.R
+++ src/rdaforest/get_env_data_v1.R
@@ -10,7 +10,9 @@ s_layer <- load_layers("MS_biogeo08_sss_mean_5m")
 # load data table with coordinates and samples (manually selected for now)
 # should do it for all samples eventually
 data_table <- read.table("data/samples_table.csv", sep = ",", header = TRUE, na.strings = c("UNKNOWN", "NA"))
+
 samples <- c("AAL1", "AAL2", "AAL3", "AF29", "AF30", "AF31", "AF8", "AK1", "AK2", "AK3", "AM27", "AM29", "AM33", "AM8", "BF16", "BF18", "BF19", "BF21", "BM14", "BM15", "BM16", "BM19", "Fehmarn3", "Fehmarn44", "Fehmarn6", "Gavle100", "Gavle54", "Gavle98", "MHER001", "MHER003", "MHER006", "MHER007", "MHER008", "MHER009", "MHER010", "MHER011", "MHER012", "MHER013", "MHER014", "MHER015", "MHER016", "MHER017", "MHER018", "MHER019", "MHER020", "MHER021", "MHER022", "MHER023", "MHER024", "MHER025", "MHER026", "MHER027", "MHER028", "MHER029", "MHER030", "MHER034", "MHER035", "MHER036", "MHER037", "MHER038", "MHER039", "MHER044", "MHER045", "MHER046", "MHER052", "MHER053", "MHER054", "MHER055", "MHER056", "MHER057", "MHER058", "MHER059", "MHER061", "MHER062", "MHER063", "MHER065", "MHER066", "NorthSea13", "NorthSea19", "NorthSea34", "NSSH33", "NSSH34", "NSSH36", "Z12", "Z14", "Z4")
+samples <- c("12S4Rsv38", "12S4Rsv41", "12S4S7i22", "12S4S7i28", "14F4Sbs344", "14F4Sbs349", "14F4TL404", "14F4TL415", "14F4WK306", "14F4WK316", "14S3Ps233", "14S3Ps265", "15F2J602", "15F2J603", "15F2J606", "15F2J616", "15F3K601", "15F3K602", "15F3K609", "15F3K621", "15F4XRsb625", "15F4XRsb626", "15F5Y514617", "15F5Y514620", "15S3K404", "15S3K410", "15S3K430", "15S3K449", "16S4Pla78", "16S4Pla79", "16S6Pla71", "16S6Pla72", "16SBDO106", "16SBDO107", "AAL1", "AAL2", "AAL3", "AF29", "AF30", "AF31", "AF8", "AK1", "AK2", "AK3", "AM27", "AM29", "AM33", "AM8", "BF16", "BF18", "BF19", "BF21", "BM14", "BM15", "BM16", "BM19", "Fehmarn3", "Fehmarn44", "Fehmarn6", "Gavle100", "Gavle54", "Gavle98", "MHER001", "MHER003", "MHER006", "MHER007", "MHER008", "MHER009", "MHER010", "MHER011", "MHER012", "MHER013", "MHER014", "MHER015", "MHER016", "MHER017", "MHER018", "MHER019", "MHER020", "MHER021", "MHER022", "MHER023", "MHER024", "MHER025", "MHER026", "MHER027", "MHER028", "MHER029", "MHER030", "MHER034", "MHER035", "MHER036", "MHER037", "MHER038", "MHER039", "MHER044", "MHER045", "MHER046", "MHER052", "MHER053", "MHER054", "MHER055", "MHER056", "MHER057", "MHER058", "MHER059", "MHER061", "MHER062", "MHER063", "MHER065", "MHER066", "NorthSea13", "NorthSea19", "NorthSea34", "NSSH33", "NSSH34", "NSSH36", "Z12", "Z14", "Z4")
 data_table <- data_table[data_table$sample_id %in% samples, ]
 # data_table <- data_table[!data_table$sample_id %in% c("MB1","MB2","MB3","MB4","MB5","MB6","MB7","MB8"), ]
 # data_table <- data_table %>%
@@ -49,6 +51,6 @@ write.table(etable, "data/rdaforest/etable.csv", sep = ",")
 # plot(etable$sst_mean, etable$y)
 # plot(etable$sss_mean, etable$x)
 
-# my_colors <- colorRampPalette(c("#5E85B8", "#EDF0C0", "#C13127"))
-# plot(crop(t_layer, extent(-25, 13, 60, 67)), col = my_colors(30), axes = T, box = F, colNA = NA)
-# points(data_table$x, data_table$y)
+my_colors <- colorRampPalette(c("#5E85B8", "#EDF0C0", "#C13127"))
+plot(crop(t_layer, extent(-80, 30, 35, 80)), col = my_colors(30), axes = T, box = F, colNA = NA)
+points(data_table$x, data_table$y)
