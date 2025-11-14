0295f5c1583a60ebb55cf9b659738676cf11da3b
Enrico Bazzicalupo <enricobazzical@gmail.com>
2025-11-14 10:40:34 +0100

    less threads

--- src/calling_and_filtering/merge_bcfs.sh
+++ src/calling_and_filtering/merge_bcfs.sh
@@ -2,16 +2,16 @@
 #SBATCH -A naiss2025-5-565
 #SBATCH -p shared
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=5
 #SBATCH -t 1-00:00:00
 #SBATCH --mem=50G
 
 module load bcftools
 
-THR=20
+THR=5
 input_dataset=${1}
 output_bcf=${2}
 
-bcftools merge -m snps -Ob --threads ${THR} -o ${output_bcf} --file-list ${input_dataset}
+bcftools merge -m snps -Ou --threads ${THR} -o ${output_bcf} --file-list ${input_dataset}
 bcftools index -o ${output_bcf}.csi ${output_bcf}
 bcftools stats ${output_bcf} > ${output_bcf}.stats
